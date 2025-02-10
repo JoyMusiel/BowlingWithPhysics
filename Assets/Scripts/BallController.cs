83299f1bfa1f74094241a4e126da553cb27340f6
Joy Musiel-Henseleit <joymusiel@gmail.com>
2025-02-10 14:54:59 -0800

    Implemented player movement

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -9,11 +9,11 @@ public class BallController : MonoBehaviour
     private bool isBallLaunched;
     void Start()
     {
-        // Grabbing a reference to RigidBody
+        // grabbing a reference to RigidBody
         ballRB = GetComponent<Rigidbody>();
 
-        // Add a listener to the OnSpacePressed event
-        // When the space key is pressed the
+        // add a listener to the OnSpacePressed event
+        // when the space key is pressed the
         // LaunchBall method will be called
         inputManager.OnSpacePressed.AddListener(LaunchBall);
     }
@@ -26,7 +26,6 @@ public class BallController : MonoBehaviour
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private float speed;
+    private Rigidbody rb;
+    void Start()
+    {
+        // add MovePlayer as a listener to the OnMove event
+        inputManager.OnMove.AddListener(MovePlayer);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void MovePlayer(Vector2 direction)
+    {
+        // listen to left and right inputs
+        Vector3 moveDirection = new(direction.x, 0f, direction.y);
+        rb.AddForce(speed * moveDirection);
+    }
+
+    void Update()
+    {
+        
+    }
+}
