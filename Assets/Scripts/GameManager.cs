32a98b57db0a624021b33db66c6643f9bc25ff0d
Joy Musiel-Henseleit <joymusiel@gmail.com>
2025-02-10 19:46:33 -0800

    Added scoring system

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private float score = 0;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    private FallTrigger[] pins;
+    private void Start()
+    {
+        // find all objects of type FallTrigger
+        pins = FindObjectsByType<FallTrigger>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+
+        // loop through array of pins and add the IncrementScore function as a listener
+        foreach (FallTrigger pin in pins)
+        {
+            pin.OnPinFall.AddListener(IncrementScore);
+        }
+    }
+
+    private void IncrementScore()
+    {
+        score++;
+        scoreText.text = $"Score: {score}";
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Gutter.cs
+++ Assets/Scripts/Gutter.cs
@@ -4,17 +4,20 @@ public class Gutter : MonoBehaviour
 {
     private void OnTriggerEnter(Collider triggeredBody)
     {
-        // get the rigidbody of the ball and store it in a local variable
-        Rigidbody ballRigidBody = triggeredBody.GetComponent<Rigidbody>();
+        if (triggeredBody.CompareTag("Ball"))
+        {
+            // get the rigidbody of the ball and store it in a local variable
+            Rigidbody ballRigidBody = triggeredBody.GetComponent<Rigidbody>();
 
-        // store the velocity magnitude before resetting the velocity
-        float velocityMagnitude = ballRigidBody.linearVelocity.magnitude;
+            // store the velocity magnitude before resetting the velocity
+            float velocityMagnitude = ballRigidBody.linearVelocity.magnitude;
 
-        // reset the linear and angular velocity
-        ballRigidBody.linearVelocity = Vector3.zero;
-        ballRigidBody.angularVelocity = Vector3.zero;
+            // reset the linear and angular velocity
+            ballRigidBody.linearVelocity = Vector3.zero;
+            ballRigidBody.angularVelocity = Vector3.zero;
 
-        // add force in the forward direction of the gutter
-        ballRigidBody.AddForce(transform.forward * velocityMagnitude, ForceMode.VelocityChange);
+            // add force in the forward direction of the gutter
+            ballRigidBody.AddForce(transform.forward * velocityMagnitude, ForceMode.VelocityChange);
+        }
     }
 }
